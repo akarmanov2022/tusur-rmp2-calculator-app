b0e267cb1aec375f419a98a99fd56e616441fb94
aka <aka@startext.dev>
2023-09-22 11:08:06 +0700

    Initial commit

--- RMP2-CalculatorApp/RMP2-CalculatorApp/App.xaml.cs
+++ RMP2-CalculatorApp/RMP2-CalculatorApp/App.xaml.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 namespace RMP2_CalculatorApp
 {
     public partial class App : Application
